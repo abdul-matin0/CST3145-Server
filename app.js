3f93002cfd08c59fc2208c8de4631a948926eb2b
Abdul-matin <madeayo04@gmail.com>
2023-02-05 18:43:31 +0000

    port num

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ app.param('collectionName'
     });
 
 /// listening on port 3000
+const port = process.env.PORT || 3000;
 app.listen(3000, function () {
-    console.log("App started on port 3000");
+    console.log("App started on port: " + port);
 });
